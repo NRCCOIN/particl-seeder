bd4c8ec05995bcd52cfb36fb1e4e541195cbbec8
NRCCOIN <noorcoin@protonmail.com>
2018-02-20 01:40:37 +0100

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 51938 : 51738;
+    return testnet ? 66010 : 65010;
 }
 
 //
